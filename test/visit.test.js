a07aa695763345583f435e0700cc731067088636
jocelyn <jocelyn@amtsolutions.fr>
2022-02-14 10:29:08 +0100

    add visit test

---
+++ test/visit.test.js
@@ -0,0 +1,8 @@
+const { getCatalogValue } = require("../lib");
+const CATALOG = require("./constant");
+
+test('Visit catalog method', () => {
+    expect(getCatalogValue(CATALOG['en']['messages'], 'hello')).toBe('Hello');
+    expect(getCatalogValue(CATALOG['en']['messages'], 'translations.are.incredible')).toBe('The translations are incredible.');
+    expect(getCatalogValue(CATALOG['en']['messages'], 'very.compound.key')).toBe('The compound key');
+});
