a5477f461f51d9f1ae417eac1c028940d9341810
jocelyn <jocelyn@amtsolutions.fr>
2022-02-15 12:32:05 +0100

    add proxy test

---
+++ test/proxy.test.js
@@ -0,0 +1,26 @@
+import Translator from '../lib';
+
+test('Default export as method', () => {
+    const translator = Translator({
+        en: { messages: { hello: 'Hello' } },
+        fr: { messages: { hello: 'Bonjour' } }
+    });
+
+    expect(translator.translate('hello')).toBe('Hello'); // => "Hello"
+    expect(translator.translate('hello', null, null, 'fr')).toBe('Bonjour'); // => "Hello"
+});
+
+test('Default export as constructor', () => {
+    const translations = new Map();
+    translations.set('messages-en', { hello: 'Hello' });
+    translations.set('messages-fr', { hello: 'Bonjour' });
+
+    const translator = new Translator(translations);
+    expect(translator.translate('hello')).toBe('Hello');
+    expect(translator.translate('hello', null, null, 'fr')).toBe('Bonjour');
+});
+
+test('Other proxy method', () => {
+    expect(() => Translator.test = true).toThrow('It\'s not allowed to add a property to Translator class, please use a composition relation instead.');
+    expect(Translator.create).toBeInstanceOf(Function);
+});
