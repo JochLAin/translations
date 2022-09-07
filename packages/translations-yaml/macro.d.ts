11ff44c86a5047f7dbd8e3486c46e84ac5cbf99c
Jochlain <jocelyn.faihy@gmail.com>
2022-09-07 17:40:14 +0200

    fix invalid type

--- packages/translations-yaml/macro.d.ts
+++ packages/translations-yaml/macro.d.ts
@@ -1,4 +1,4 @@
 import { Translator } from "@jochlain/translations";
 
 export function translate(key: string, replacements: { [key: string]: number|string }, options: { domain?: string, locale?: string, host?: string }): string;
-export function createTranslator(options?: string|{ host?: string }): Translator;
+export function createTranslator(options?: string|{ host?: string }): typeof Translator;
