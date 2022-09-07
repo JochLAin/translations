2479ff21ede3d28292ed315d31735e372e7709ff
Jochlain <jocelyn.faihy@gmail.com>
2022-09-07 17:38:12 +0200

    add macro types

---
+++ packages/translations-json/macro.d.ts
@@ -0,0 +1,4 @@
+import { Translator } from "@jochlain/translations";
+
+export function translate(key: string, replacements: { [key: string]: number|string }, options: { domain?: string, locale?: string, host?: string }): string;
+export function createTranslator(options?: string|{ host?: string }): Translator;
